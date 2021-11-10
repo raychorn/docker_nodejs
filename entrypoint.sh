43f651fc99e534fef90bb6a4595a9b5e1d479203
Ray C Horn <raychorn@gmail.com>
2021-11-10 15:41:07 -0700

    updates.

--- entrypoint.sh
+++ entrypoint.sh
@@ -92,7 +92,7 @@ rm -rf fonts
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k/powerlevel10k"/' "~/.zshrc"
+#sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k/powerlevel10k"/' "~/.zshrc"
 
 nodejs --verrsion
 npm --version
